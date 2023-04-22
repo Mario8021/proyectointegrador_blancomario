86b7f77eef361bf91762580f186e85937145b066
Mario8021 <88853720+Mario8021@users.noreply.github.com>
2023-04-22 18:10:15 -0300

    front

---
+++ proyectointegrador_blancomario/src/app/components/hysskills/hysskills.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hysskills',
+  templateUrl: './hysskills.component.html',
+  styleUrls: ['./hysskills.component.css']
+})
+export class HysskillsComponent {
+
+}
